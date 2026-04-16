afd836a350f983d93fc3fae37428edb1b3d98634
Silas Maughan <silas.maughan@gmail.com>
2026-04-16 11:26:18 +0200

    add yank file binding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,9 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live gr
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
+vim.keymap.set("n", "<leader>yf", function()
+    vim.fn.setreg("+", vim.fn.expand("%:."))
+end, { desc = "Yank relative path" })
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
 -- Basic Settings
